a2843b3894ac2faa2ea3d12b247d5c8861e7f4ab
cedylad <cedricanassi@gmail.com>
2023-08-09 02:59:26 +0200

    Initial Commit

---
+++ Formation/FormationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormationApp.swift
+//  Formation
+//
+//  Created by Cedy on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FormationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
